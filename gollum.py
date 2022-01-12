bbdaf6c5f1fa770c6bf2ab8b8541a73907c24bc8
Werner Mueller <werner.mueller@monis.ch>
2022-01-12 14:32:22 +0100

    added command to call wowbagger and Mäh

--- gollum.py
+++ gollum.py
@@ -6,9 +6,32 @@ from datetime import datetime
 
 class GollumBot(Plugin):
 
+    @command.new(name="beleidige", aliases=["b"])
+    @command.argument("names", pass_raw=True, required=False)
+    async def beleidige(self, evt: MessageEvent, names: str) -> None:
+        await evt.mark_read()
+        await evt.reply("jezz goots ab!")
+        self.log.info("Wär: " + names)
+        if not names:
+            namen = ""
+        else:
+            namen = names
+
+        async with self.http.get(
+                "https://swiss-wowbagger-ultgi7by3q-oa.a.run.app/?format=json&v=undefined&names={}&voice=".format(namen)
+        ) as response:
+            data = await response.json()
+
+        self.log.info(data)
+        await self.client.send_text(evt.room_id, data["text"])
+
+    @command.new(name="mäh")
+    async def maeh(self, evt: MessageEvent) -> None:
+        await self.client.send_markdown(evt.room_id, "__Mas Tequila!!__")
+
     @command.new(name="lion")
     async def lion(self, evt: MessageEvent) -> None:
-        await evt.reply("Löwe kommt! ")
+        await evt.reply("Löwe kommt!")
         await self.send_image(evt.room_id, "https://www.tolkienforum.de/stuff/botme/lions.php", "lion")
 
     @command.new(name="milooo")
